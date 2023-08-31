3597ce18e4f10a372df5cfc1f106740829d6fa08
MurdoXu <112531563+MurdoXu@users.noreply.github.com>
2023-08-31 19:32:02 +1000

    Blink.ino

--- Blink.ino
+++ Blink.ino
@@ -1,15 +1,7 @@
-// Teensy 2.0 has the LED on pin 11
 const int ledPin = 11;
-
-// the setup() method runs once, when the sketch starts
-
 void setup() {
-  // initialize the digital pin as an output.
   pinMode(ledPin, OUTPUT);
 }
-
-// the loop() methor runs over and over again,
-// as long as the board has power
 
 void loop() {
   digitalWrite(ledPin, HIGH);   // set the LED on
